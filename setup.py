63435df7a3dd011cc28493cf8d4c810c018909cc
R Steiner <r.steiner@uconn.edu>
2019-03-06 16:29:02 -0500

    Move most corpus files out of package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = "0.1"
+
+setup(
+    name="ungen",
+    version=version,
+    description="Generate usernames from pseudo-random strings of words.",
+    url="https://github.com/cranndarach/username_generaotr",
+    author="R. J. Steiner",
+    license="MIT",
+    packages=["ungen"]
+)
