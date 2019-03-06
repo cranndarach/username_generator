9e5e9d4750ad3218127d5fa3085872b3d119a4b5
R Steiner <r.steiner@uconn.edu>
2019-03-06 16:48:55 -0500

    Make verbose output more descriptive.

--- ungen/usernames.py
+++ ungen/usernames.py
@@ -19,27 +19,30 @@ def make_name(*args):
 
 def make_some_names(n, verbose=False):
     abbrevs = {
-        "an": [corpus.adjectives, corpus.nouns],
-        "aan": [corpus.adjectives, corpus.adjectives, corpus.nouns],
-        "aa": [corpus.adverbs, corpus.adjectives],
-        "adn": [corpus.adverbs, corpus.determiners, corpus.nouns],
-        "vp": [corpus.verbs, corpus.prepositions],
-        "dan": [corpus.determiners, corpus.adjectives, corpus.nouns],
-        "aca": [corpus.adjectives, corpus.conjunctions, corpus.adjectives],
-        "va": [corpus.verbs, corpus.adverbs],
-        "vpn": [corpus.verbs, corpus.prepositions, corpus.nouns],
-        "npn": [corpus.nouns, corpus.prepositions, corpus.nouns],
-        "nn": [corpus.nouns, corpus.nouns],
-        "pdn": [corpus.prepositions, corpus.determiners, corpus.nouns],
-        "vcv": [corpus.verbs, corpus.conjunctions, corpus.verbs],
-        "ncn": [corpus.nouns, corpus.conjunctions, corpus.nouns]
+        "adj-noun":      [corpus.adjectives, corpus.nouns],
+        "adj-adj-noun":  [corpus.adjectives, corpus.adjectives, corpus.nouns],
+        "adv-adj":       [corpus.adverbs, corpus.adjectives],
+        "adv-det-noun":  [corpus.adverbs, corpus.determiners, corpus.nouns],
+        "verb-prep":     [corpus.verbs, corpus.prepositions],
+        "det-adj-noun":  [corpus.determiners, corpus.adjectives, corpus.nouns],
+        "adj-conj-adj":  [corpus.adjectives, corpus.conjunctions,
+                          corpus.adjectives],
+        "verb-adv":      [corpus.verbs, corpus.adverbs],
+        "verb-prep-noun": [corpus.verbs, corpus.prepositions, corpus.nouns],
+        "noun-prep-noun": [corpus.nouns, corpus.prepositions, corpus.nouns],
+        "noun-noun":     [corpus.nouns, corpus.nouns],
+        "prep-det-noun": [corpus.prepositions, corpus.determiners,
+                          corpus.nouns],
+        "verb-conj-verb": [corpus.verbs, corpus.conjunctions, corpus.verbs],
+        "noun-conj-noun": [corpus.nouns, corpus.conjunctions, corpus.nouns]
     }
     types = list(abbrevs.keys())
     # if not types:
     #     types = abbrevs.keys()
     for _ in range(n):
         selected = rd.choice(types)
-        if verbose:
-            print(selected)
+        # if verbose:
+        #     print(selected)
         combo = abbrevs.get(selected, selected)
-        print(make_name(*combo))
+        info = f" ({selected})" if verbose else ""
+        print(make_name(*combo), info, sep="")
